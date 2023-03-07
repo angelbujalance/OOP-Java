b053f40892f4872662c06f9d65b9f5c92832f38a
angelbujalance <angelbujalanceg@gmail.com>
2023-03-07 23:16:59 +0100

    PAC1

---
+++ PAC1Ex2/src/PAC1Ex2.java
@@ -0,0 +1,22 @@
+
+package edu.uoc.pac1;
+
+public class PAC1Ex2 {
+
+    public static double twoDecimals(double number){
+        return Math.round(number * 100d) / 100d;
+    }
+
+    public static int incomeTaxRate(double amount){
+        //TODO
+    }
+
+    public static double totalIncomeTaxVAT(double amount) {
+        //TODO
+    }
+
+    public static double[] invoicesTotal(double[][] invoices) {
+        //TODO
+    }
+
+}
